8e14c73fd6e371c25ac665bebc761820d27d2322
soblab <soblab@Whit209-14.it.farmingdale.edu>
2023-03-29 13:23:24 -0400

    1.0

--- Lab8-SinglyLinkedList/src/main/java/SinglyLinkedList.java
+++ Lab8-SinglyLinkedList/src/main/java/SinglyLinkedList.java
@@ -8,6 +8,11 @@
  *
  * @author soblab
  */
+/*  Write a program that uses a SinglyLinkedList that gets populated by prime numbers from 0 to n. 
+    Then move the prime numbers that has the digit '3' into another linked list. 
+    After that find the sum of numbers in the second list.  (n should be less than 1,000,000).
+    For example, the sum of the prime numbers (with 3 digit) between 0 and n=100 is (3+ 13+  23+ 31+ 37+ 43+  53+73+83 )= 359.
+*/  
 public class SinglyLinkedList {
     
 }
